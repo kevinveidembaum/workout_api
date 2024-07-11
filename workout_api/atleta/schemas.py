c8d12a811370758b93413f4f2e0f768ee87659f0
Kevin Nikolai <nikolai10052005@gmail.com>
2024-07-11 20:27:14 -0300

    feat: first commit

---
+++ workout_api/atleta/schemas.py
@@ -0,0 +1,11 @@
+from typing import Annotated
+from pydantic import BaseModel, Field, PositiveFloat
+
+
+class AtletaSchema(BaseModel):
+    nome: Annotated[str, Field(description='Nome do atleta', examples='Nikolai', max_length=50)]
+    cpf: Annotated[str, Field(description='CPF do atleta', examples='12345678900', max_length=11)]
+    idade: Annotated[int, Field(description='Idade do atleta', examples=20)]
+    peso: Annotated[PositiveFloat, Field(description='Peso do atleta', examples=109.4)]
+    altura: Annotated[PositiveFloat, Field(description='Altura do atleta', examples=1.88)]
+    sexo: Annotated[str, Field(description='Sexo do atleta', examples='M', max_length=1)]
