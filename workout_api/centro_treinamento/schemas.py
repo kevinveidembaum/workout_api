64511b5bb444d08554c56bcf4a872aebd2a35793
Kevin Nikolai <nikolai10052005@gmail.com>
2024-07-12 19:56:03 -0300

    fix: swagger and endpoints

--- workout_api/centro_treinamento/schemas.py
+++ workout_api/centro_treinamento/schemas.py
@@ -1,10 +1,18 @@
 from typing import Annotated
 
-from pydantic import Field
+from pydantic import Field, UUID4
 from workout_api.contrib.schemas import BaseSchema
 
 
-class CentroTreinamentoSchema(BaseSchema):
+class CentroTreinamentoIn(BaseSchema):
     nome: Annotated[str, Field(description='Nome do centro de treinamento', example='CT Iron Berg', max_length=20)]
-    endereco: Annotated[str, Field(description='Endereço do centro de treinamento', example='Rua X, Q10', max_length=60)]
-    proprietario: Annotated[str, Field(description='Proprietário do centro de treinamento', example='Cariri', max_length=30)]
+    endereco: Annotated[str, Field(description='Endereço do centro de treinamento', example='Rua X, Q12', max_length=60)]
+    proprietario: Annotated[str, Field(description='Proprietario do centro de treinamento', example='Cariri', max_length=30)]
+
+
+class CentroTreinamentoAtleta(BaseSchema):
+    nome: Annotated[str, Field(description='Nome do centro de treinamento', example='CT Iron Berg', max_length=20)]
+
+
+class CentroTreinamentoOut(CentroTreinamentoIn):
+    id: Annotated[UUID4, Field(description='Identificador do centro de treinamento')]    
